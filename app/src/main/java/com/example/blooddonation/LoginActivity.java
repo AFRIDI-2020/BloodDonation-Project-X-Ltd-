6fa4d2a1c4b0a997090becc3ff9f648fef6cb4b3
afridi <afridi.mahafuz@gmail.com>
2021-03-31 09:39:10 +0600

    signup1,2,3 design complete

--- app/src/main/java/com/example/blooddonation/LoginActivity.java
+++ app/src/main/java/com/example/blooddonation/LoginActivity.java
@@ -2,13 +2,15 @@ package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView forgetPassTV;
+    private TextView forgetPassTV,signUPTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         init();
+
+        signUPTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUp1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void init() {
         forgetPassTV = findViewById(R.id.tv_forget_password);
+        signUPTV = findViewById(R.id.signUpTV);
         forgetPassTV.setPaintFlags(forgetPassTV.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
